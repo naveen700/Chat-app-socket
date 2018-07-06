47e2a0ecd87bf3ef06b8a0b73b59a83d5395fdf6
Your Name <naveenrana921@gmail.com>
2018-07-06 14:59:44 +0530

    second

--- server/server.js
+++ server/server.js
@@ -1,27 +1,37 @@
-const express  = require('express');
+// https://infinite-savannah-35646.herokuapp.com/
+const express = require('express');
 //path  is  better api to handle the paths
 const path = require('path');
+const http = require('http');
 // console.log(__dirname + '../public/index.html');  // old way to resolve path
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname,'../public');
+const socketIO = require('socket.io');
+const publicPath = path.join(__dirname, '../public');
 // console.log(publicPath);
-
-
 
 var app = express();
 
+var server = http.createServer(app);
+
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-1
+
+io.on('connection', (socket) => { // when client gets connected
 
 
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user is disconnected');
+    });
 
-
-
-
-app.listen(port, () =>{
-
-    console.log('server started at 3000',port);
 
 })
 
+
+
+server.listen(port, () => {
+
+    console.log('server started at 3000', port);
+
+})
